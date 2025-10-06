a402c6c32d2f8f64f7de23f800d8e9117418e998
Suraiya P Mark <s.pobmark@gmail.com>
2025-10-05 22:22:19 -0400

    first chunk done, part 3 not yet

--- HWs/HW1/hw1.c
+++ HWs/HW1/hw1.c
@@ -1,3 +1,5 @@
+// Suraiya Poberezhsky Mark, EMPLID: 23756247
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -6,29 +8,31 @@
 
 #define READ_END 0
 #define WRITE_END 1
+// The definitions make sense for consistency, and I'm guessing are industry-standard, but they feel a bit long to me (for what they are).
+// I put those here since they are in the examples in the textbook, so I imagine are a good practice.
 
 int main(void) {
     int fd[2];
     pid_t pid1, pid2;
 
-    if (pipe(fd)==-1) {
+    if (pipe(fd)==-1) { // Single direction communication for the processes.
         perror("pipe issue");
         exit(1);
     }
-    pid1=fork();
-    if (pid1<0) {
-        perror("fork1 issue");
+    pid1=fork(); // duplicates into new process. Runs same as parent, but with the pid value logic. The child gets 0, parent gets a positive int for the child's pid.
+    if (pid1<0) { // child not made
+        perror("fork1 issue"); // Writing it to std error: https://manpages.ubuntu.com/manpages/questing/en/man3/perror.3posix.html
         exit(1);
     }
-    if (pid1==0) {
-        close(fd[READ_END]);
-        dup2(fd[WRITE_END], STDOUT_FILENO);
+    if (pid1==0) { // child is active
+        close(fd[READ_END]); //closes file descrip., so read end here since not needed by the first child (first writes, second reads)
+        dup2(fd[WRITE_END], STDOUT_FILENO); // duplicates file descriptor, uses specified one
         close(fd[WRITE_END]);
 
         char* comm1 = "ls";
         char* args1[] = {"ls", "-F", NULL};
 
-        int stat = execvp(comm1, args1);
+        int stat = execvp(comm1, args1); // replaces current proc image with new https://man7.org/linux/man-pages/man3/exec.3.html
         if (stat==-1) {
             perror("command 1 issue, status");
             exit(1);
@@ -56,7 +60,23 @@ int main(void) {
     close(fd[READ_END]);
     close(fd[WRITE_END]);
 
-    waitpid(pid1, NULL, 0);
+    waitpid(pid1, NULL, 0); // suspends exec of calling thread until child specified by pid arg has changed state https://manpages.ubuntu.com/manpages/questing/en/man2/waitpid.2.html
     waitpid(pid2, NULL, 0);
     return 0;
 }
+/*
+
+Misc links used (pretty much entirely from links from the assignment sheet, the manpages, or echo file):
+
+https://man7.org/linux/man-pages/man3/exec.3.html
+https://manpages.ubuntu.com/manpages/questing/en/man2/waitpid.2.html
+https://manpages.ubuntu.com/manpages/noble/en/man3/access.3posix.html
+https://manpages.ubuntu.com/manpages/questing/en/man2/access.2freebsd.html
+https://manpages.ubuntu.com/manpages/questing/en/man3/perror.3posix.html
+https://cs.brown.edu/courses/csci0300/2022/notes/l19.html
+https://manpages.ubuntu.com/manpages/questing/en/man7/pipe.7.html
+https://www.digitalocean.com/community/tutorials/execvp-function-c-plus-plus
+https://www.dropbox.com/scl/fo/3z5ot6qr99itnuupkosb3/ACKitIh13atPpHwSetWeYXQ/source%20code?e=1&preview=echo.txt&rlkey=ey74b0tvsw3gkzahense6700n&subfolder_nav_tracking=1&dl=0
+(then just lecture slides and textbook itself)
+
+*/
